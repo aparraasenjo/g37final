f2216d4b2fbc5e59135a725a7eae8071898c652c
Alberto Parra <a.parra.asenjo@gmail.com>
2021-07-19 20:35:15 -0400

    added devise to producer and designer models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_002129) do
+ActiveRecord::Schema.define(version: 2021_07_20_003425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,26 @@ ActiveRecord::Schema.define(version: 2021_07_20_002129) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_designers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_designers_on_reset_password_token", unique: true
   end
 
   create_table "producers", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_producers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_producers_on_reset_password_token", unique: true
   end
 
 end
