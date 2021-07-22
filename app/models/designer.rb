fb1648eb2a201010ff1750c4b05084663089cc7f
Alberto Parra <a.parra.asenjo@gmail.com>
2021-07-21 20:58:55 -0400

    added to_s method to model producer and designer

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,5 +1,9 @@
 class Designer < ApplicationRecord
   has_many :works
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def to_s
+    name
+  end
 end
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -2,4 +2,7 @@ class Producer < ApplicationRecord
   has_many :tracks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def to_s
+    name
+  end
 end
