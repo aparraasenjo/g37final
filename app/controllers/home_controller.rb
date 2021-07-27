25043c1d4ca474580450fd45501fb49e868728d8
Alberto Parra <a.parra.asenjo@gmail.com>
2021-07-26 22:39:47 -0400

    add search with ujs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    if params[:search]
+      @search = Track.tagged_with(params[:search].split(' '))
+
+      respond_to do |format|
+        format.html
+        format.js 
+      end
+    else
+    end
   end
 end
