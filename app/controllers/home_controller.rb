7ce0d21f4f719465a51036ed771294e012a30769
Alberto Parra <a.parra.asenjo@gmail.com>
2021-07-28 19:55:15 -0400

    added search for both models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,19 @@
 class HomeController < ApplicationController
   def index
-    if params[:search]
+    if params[:search] && designer_signed_in? 
       @search = Track.tagged_with(params[:search].split(' '))
 
       respond_to do |format|
         format.html
         format.js 
       end
+    elsif params[:search] && producer_signed_in? 
+        @search = Work.tagged_with(params[:search].split(' '))
+  
+        respond_to do |format|
+          format.html
+          format.js 
+        end
     else
     end
   end
